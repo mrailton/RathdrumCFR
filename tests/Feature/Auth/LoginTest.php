b99835dc7131c74137e4a5db2a9a911e7ac2c034
Mark Railton <marksrailton@gmail.com>
2022-11-24 18:29:33 +0000

    Merge tests

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -6,6 +6,22 @@ use App\Models\User;
 
 use function Pest\Laravel\get;
 use function Pest\Laravel\post;
+use function Pest\Laravel\actingAs;
+
+test('login page renders', function () {
+    get(route('login'))
+        ->assertSee('Rathdrum Community First Responders')
+        ->assertSee('Sign in to your account')
+        ->assertSee('Email address');
+});
+
+test('an authenticated user can not visit the login page', function () {
+    actingAs(User::factory()->create());
+
+    get(route('login'))
+        ->assertStatus(302)
+        ->assertRedirectToRoute('index');
+});
 
 it('allows a user to login', function () {
     $user = User::factory()->create();
