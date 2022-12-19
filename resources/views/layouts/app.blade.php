4c6b27209bf99f4c3e4fa1a00002382405bade13
Mark Railton <marksrailton@gmail.com>
2022-12-19 19:33:44 +0000

    Add plausible

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Rathdrum Community First Responders</title>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @production
+        <script defer data-domain="rathdrumcfr.com" src="https://analytics.markrailton.com/js/plausible.js"></script>
+    @endproduction
+
+    <title>Rathdrum Community First Responders</title>
 </head>
 <body class="flex flex-col min-h-screen font-sans antialiased">
 <x-header />
