60f2d23bec22033cd90fc9c3d1d8671cf7505c54
Mark Railton <marksrailton@gmail.com>
2023-01-22 14:33:12 +0000

    Add validation errors to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,10 @@
                     <div>
                         <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
                         <div class="mt-1">
-                            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
+                            <input id="email" name="email" type="email" autocomplete="email" required class="block @error('email') border-red-500 @enderror w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
+                            @error('email')
+                                <div class="text-sm text-red-600">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
 
