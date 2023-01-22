62077422c2f1e6271c865399619a1d68e5bf2d65
Mark Railton <marksrailton@gmail.com>
2023-01-22 15:24:06 +0000

    Adds validation messages to member update and note

--- app/Http/Requests/Members/Notes/StoreMemberNoteRequest.php
+++ app/Http/Requests/Members/Notes/StoreMemberNoteRequest.php
@@ -22,7 +22,7 @@ class StoreMemberNoteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'note' => ['string', 'required'],
+            'note' => ['required', 'string'],
         ];
     }
 }
