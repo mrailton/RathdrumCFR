4da47fa73f7d6807fc5c182c828c1a962fa3f8e6
Mark Railton <marksrailton@gmail.com>
2023-02-04 15:01:05 +0000

    convert page tests to pest

--- tests/Feature/ContactUsTest.php
+++ tests/Feature/ContactUsTest.php
@@ -2,44 +2,34 @@
 
 declare(strict_types=1);
 
-namespace Tests\Feature;
-
 use App\Mail\ContactFormMail;
 use Illuminate\Support\Facades\Mail;
 use Lunaweb\RecaptchaV3\Facades\RecaptchaV3;
-use Tests\TestCase;
 
-class ContactUsTest extends TestCase
-{
-    /** @test */
-    public function the_contact_us_form_renders(): void
-    {
-        $this->get(route('contact.create'))
-            ->assertSee('Rathdrum Community First Responders')
-            ->assertSee('please fill in the form below and someone will get back to you as soon as possible.')
-            ->assertSee('Name')
-            ->assertSee('Contact Us');
-    }
+test('the contact us form renders', function () {
+    $this->get(route('contact.create'))
+        ->assertSee('Rathdrum Community First Responders')
+        ->assertSee('please fill in the form below and someone will get back to you as soon as possible.')
+        ->assertSee('Name')
+        ->assertSee('Contact Us');
+});
 
-    /** @test */
-    public function the_contact_form_can_be_submitted(): void
-    {
-        Mail::fake();
-        RecaptchaV3::shouldReceive('verify')
-            ->once()
-            ->andReturn(1.0);
+test('the contact form can be submitted', function () {
+    Mail::fake();
+    RecaptchaV3::shouldReceive('verify')
+        ->once()
+        ->andReturn(1.0);
 
-        $formData = [
-            'name' => 'Test User',
-            'email' => 'test@user.com',
-            'phone' => '0831234567',
-            'message' => 'This is a test',
-            'g-recaptcha-response' => '1'
-        ];
+    $formData = [
+        'name' => 'Test User',
+        'email' => 'test@user.com',
+        'phone' => '0831234567',
+        'message' => 'This is a test',
+        'g-recaptcha-response' => '1'
+    ];
 
-        $this->post(route('contact.store'), $formData)
-            ->assertRedirect(route('index'));
+    $this->post(route('contact.store'), $formData)
+        ->assertRedirect(route('index'));
 
-        Mail::assertQueued(ContactFormMail::class);
-    }
-}
+    Mail::assertQueued(ContactFormMail::class);
+});
--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -2,20 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Tests\Feature;
-
-use Tests\TestCase;
-
-class IndexTest extends TestCase
-{
-    /** @test */
-    public function index_page_loads(): void
-    {
-        $this->get(route('index'))
-            ->assertStatus(200)
-            ->assertSee('Rathdrum Community First Responders')
-            ->assertSee('We are currently working on a new website')
-            ->assertSee('but in the mean time you can view a map of our available defibrillators in the Rathdrum area')
-            ->assertSee('Login');
-    }
-}
+test('index page loads', function () {
+    $this->get(route('index'))
+        ->assertStatus(200)
+        ->assertSee('Rathdrum Community First Responders')
+        ->assertSee('We are currently working on a new website')
+        ->assertSee('but in the mean time you can view a map of our available defibrillators in the Rathdrum area')
+        ->assertSee('Login');
+});
