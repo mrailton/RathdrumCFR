c5588acef90932c4d7b356f8e72b4bb0988918f5
Mark Railton <marksrailton@gmail.com>
2023-02-04 17:01:21 +0000

    Update permissions controller test

--- tests/Feature/Users/ManageUserPermissionsTest.php
+++ tests/Feature/Users/ManageUserPermissionsTest.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 use App\Models\User;
+use Spatie\Permission\Models\Permission;
 
-test('an authorised user can view a users permissions', function () {
-    authenticatedUser(['user.view',  'user.permissions']);
+test('an authorised user can update a users permissions', function () {
+    authenticatedUser(['user.view', 'user.permissions']);
     $user = User::factory()->create();
 
     $this->get(route('users.show', ['user' => $user]))
@@ -13,4 +14,14 @@ test('an authorised user can view a users permissions', function () {
 
     $this->get(route('users.permissions.show', ['user' => $user]))
         ->assertSee('Manage Permissions for ' . $user->name);
+
+    $permissions = Permission::inRandomOrder()->limit(5)->get()->map(function ($permission) {
+        return $permission->id;
+    });
+
+    $data = ['permissions' => $permissions->toArray()];
+
+    $this->put(route('users.permissions.update', ['user' => $user]), $data)
+        ->assertSessionDoesntHaveErrors()
+        ->assertRedirectToRoute('users.show', ['user' => $user]);
 });
