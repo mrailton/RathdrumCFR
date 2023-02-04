a4d0bfb5c44f1e938d29adfae80e4b24f683c674
Mark Railton <marksrailton@gmail.com>
2023-02-04 14:55:52 +0000

    convert user tests to pest

--- tests/Feature/Users/ListUsersTest.php
+++ tests/Feature/Users/ListUsersTest.php
@@ -2,21 +2,13 @@
 
 declare(strict_types=1);
 
-namespace Tests\Feature\Users;
-
 use App\Models\User;
-use Tests\TestCase;
 
-class ListUsersTest extends TestCase
-{
-    /** @test */
-    public function an_authorised_user_can_view_a_list_of_users(): void
-    {
-        $this->actingAs($this->user(['user.list']));
-        $users = User::factory()->count(2)->create();
+test('an authorised user can view a list of users', function () {
+    authenticatedUser(['user.list']);
+    $users = User::factory()->count(2)->create();
 
-        $this->get(route('users.list'))
-            ->assertSee($users[0]->name)
-            ->assertSee($users[1]->name);
-    }
-}
+    $this->get(route('users.list'))
+        ->assertSee($users[0]->name)
+        ->assertSee($users[1]->name);
+});
--- tests/Feature/Users/ManageUserPermissionsTest.php
+++ tests/Feature/Users/ManageUserPermissionsTest.php
@@ -2,23 +2,15 @@
 
 declare(strict_types=1);
 
-namespace Tests\Feature\Users;
-
 use App\Models\User;
-use Tests\TestCase;
 
-class ManageUserPermissionsTest extends TestCase
-{
-    /** @test */
-    public function an_authorised_user_can_view_a_users_permissions(): void
-    {
-        $this->actingAs($this->user(['user.view',  'user.permissions']));
-        $user = User::factory()->create();
+test('an authorised user can view a users permissions', function () {
+    authenticatedUser(['user.view',  'user.permissions']);
+    $user = User::factory()->create();
 
-        $this->get(route('users.show', ['user' => $user]))
-            ->assertSee('Manage Permissions');
+    $this->get(route('users.show', ['user' => $user]))
+        ->assertSee('Manage Permissions');
 
-        $this->get(route('users.permissions.show', ['user' => $user]))
-            ->assertSee('Manage Permissions for ' . $user->name);
-    }
-}
+    $this->get(route('users.permissions.show', ['user' => $user]))
+        ->assertSee('Manage Permissions for ' . $user->name);
+});
