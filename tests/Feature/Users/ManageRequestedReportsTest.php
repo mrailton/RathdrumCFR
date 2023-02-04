e190a55af1e9b39164ec1745507346f7bbf1d653
Mark Railton <marksrailton@gmail.com>
2023-02-04 16:41:00 +0000

    Adds test for managing requested reports

---
+++ tests/Feature/Users/ManageRequestedReportsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+
+test('an authorised user can modify the reports they receive', function () {
+    authenticatedUser(['user.view', 'user.update']);
+    $user = User::first();
+
+    $this->get(route('users.show', ['user' => $user]))
+        ->assertSee('Requested Reports');
+
+    $this->get(route('users.reports.show', ['user' => $user]))
+        ->assertSee('User Requested Reports')
+        ->assertSee('Update');
+
+    $this->put(route('users.reports.store', ['user' => $user]), ['cfr_cert_expiry' => 'Yes'])
+        ->assertSessionDoesntHaveErrors()
+        ->assertRedirectToRoute('users.show', ['user' => $user]);
+
+});
