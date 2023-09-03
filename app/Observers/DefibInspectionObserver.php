efe9d1dc283562fb244515862ca63b4f2786fee1
Mark Railton <marksrailton@gmail.com>
2023-09-03 22:40:18 +0100

    Make sure defib inspected mail still works

--- app/Observers/DefibInspectionObserver.php
+++ app/Observers/DefibInspectionObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Events\DefibInspected;
 use App\Models\DefibInspection;
 
 class DefibInspectionObserver
@@ -14,5 +15,7 @@ class DefibInspectionObserver
             'last_inspected_at' => $inspection->inspected_at,
             'last_inspected_by' => $inspection->member->name,
         ]);
+
+        DefibInspected::dispatch($inspection->defib, $inspection);
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,16 +6,11 @@ namespace App\Providers;
 
 use App\Events\DefibInspected;
 use App\Listeners\SendDefibInspectedMail;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
         DefibInspected::class => [
             SendDefibInspectedMail::class,
         ],
