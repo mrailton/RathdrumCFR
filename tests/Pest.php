9814db83390d1361e571d9a8bca45a84351f7926
Mark Railton <marksrailton@gmail.com>
2023-12-10 22:02:47 +0000

    updating app

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,16 +3,12 @@
 declare(strict_types=1);
 
 use App\Models\User;
-use Database\Seeders\PermissionsSeeder;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
 uses(Tests\TestCase::class)->in('Feature');
 uses(LazilyRefreshDatabase::class)->in('Feature');
-uses()->beforeEach(fn () => $this->seed(PermissionsSeeder::class))->in('Feature');
 
-expect()->extend('toBeOne', fn () => $this->toBe(1));
-
-function authenticatedUser(?array $permissions = null): Tests\TestCase
+function authenticatedUser(): Tests\TestCase
 {
     $data = [
         'name' => 'Test User',
@@ -21,12 +17,6 @@ function authenticatedUser(?array $permissions = null): Tests\TestCase
     ];
 
     $user = User::first() ?: User::create($data);
-
-    if ($permissions) {
-        foreach ($permissions as $permission) {
-            $user->givePermissionTo($permission);
-        }
-    }
 
     return test()->actingAs($user);
 }
