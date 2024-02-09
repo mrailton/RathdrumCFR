c3454935d764a0787a1f67ae355799f6ae0782bd
Mark Railton <marksrailton@gmail.com>
2024-02-09 19:33:41 +0000

    Re-add backup notifications but disable

--- config/backup.php
+++ config/backup.php
@@ -153,9 +153,12 @@ return [
     'notifications' => [
 
         'notifications' => [
-            \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['mail'],
+            Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => ['mail'],
+            Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['mail'],
+            Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['mail'],
+            Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => [],
+            Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => [],
+            Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => [],
         ],
 
         /*
