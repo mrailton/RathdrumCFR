402e500036d6bb8d09175082cf15c09f5c8511fe
Shift <shift@laravelshift.com>
2024-08-09 07:44:14 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,24 +7,6 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'admin_email' => env('ADMIN_EMAIL'),
 
