0d54fc6beb120dadc935bcc9d3ce7f4a5d01d68c
Mark Railton <mark.railton@outlook.ie>
2025-09-19 23:08:31 +0100

    Override ssl for MySQL

--- config/database.php
+++ config/database.php
@@ -4,6 +4,9 @@ return [
     'connections' => [
         'mysql' => [
             'url' => env('DATABASE_URL', 'mysql://user:password@localhost/database'),
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', true),
+            ],
         ]
     ],
     'migrations' => [
