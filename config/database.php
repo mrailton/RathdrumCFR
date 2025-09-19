a27d89f32a80b54dfb2b7b4414b8819c2be66217
Mark Railton <mark.railton@outlook.ie>
2025-09-19 23:21:08 +0100

    still trying

--- config/database.php
+++ config/database.php
@@ -5,6 +5,7 @@ return [
         'mysql' => [
             'url' => env('DATABASE_URL', 'mysql://user:password@localhost/database'),
             'options' => [
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode="STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"',
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
                 PDO::MYSQL_ATTR_SSL_CA => null,
             ],
