98bd20eec54d92d87565f8dfce7ea4d82607302a
Mark Railton <mark.railton@outlook.ie>
2026-02-15 18:34:28 +0000

    fix migratinos

--- database/migrations/2024_02_05_105533_create_filament-jobs-monitor_table.php
+++ database/migrations/2024_02_05_105533_create_filament-jobs-monitor_table.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('queue_monitors', function (Blueprint $table) {
+        Schema::create('queue_monitors', function (Blueprint $table): void {
             $table->id();
             $table->string('job_id')->index();
             $table->string('name')->nullable();
--- database/migrations/2024_03_18_123259_add_lot_numbers_to_defibs.php
+++ database/migrations/2024_03_18_123259_add_lot_numbers_to_defibs.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('defibs', function (Blueprint $table) {
+        Schema::table('defibs', function (Blueprint $table): void {
             $table->string('defib_lot_number')->nullable();
             $table->date('defib_manufacture_date')->nullable();
             $table->string('battery_lot_number')->nullable();
@@ -26,7 +27,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('defibs', function (Blueprint $table) {
+        Schema::table('defibs', function (Blueprint $table): void {
             $table->dropColumn('defib_lot_number');
             $table->dropColumn('defib_manufacture_date');
             $table->dropColumn('battery_lot_number');
